2ab5b8eb14cc56f75a873ee343c4f4dbbf4f52df
AdaoBJr <adaobjr@gmail.com>
2023-04-22 23:54:59 -0400

    feat: :sparkles: created home / details.ts

---
+++ src/app/home/home.ts
@@ -0,0 +1,36 @@
+export {};
+// import { useMakeupApi } from '../../services';
+
+// const { getProducts } = useMakeupApi();
+
+// const {data: resProducts} = await getProducts();
+// console.log('res', res);
+
+// const products = document.querySelector("products")!;
+
+// resProducts.data.forEach((item) => {
+//   products.innerHTML += `<tr>
+//     <th scope="row">${responseEvents.indexOf(item) + 1}</th>
+//     <td>${new Date(item.scheduled).toLocaleDateString("pt-BR")}
+//       </td>
+//     <td>${item.name}</td>
+//     <td>${item.attractions}</td>
+//     <td>
+//         <a href="reservas.html?id=${
+//           item._id
+//         }" class="btn btn-dark">ver reservas</a>
+//         <a href="editar-evento.html?id=${
+//           item._id
+//         }" class="btn btn-secondary">editar</a>
+//         <a href="excluir-evento.html?id=${
+//           item._id
+//         }" class="btn btn-danger">excluir</a>
+//     </td>
+// </tr>`;
+// });
+
+// products.innerHTML += `
+//   <div>
+//     <div></div>
+//   </div>
+// `
