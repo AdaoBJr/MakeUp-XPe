42a7458a1d501ffba80b0ddae73128ec15d3cef4
AdaoBJr <adaobjr@gmail.com>
2023-04-23 22:01:24 -0400

    feat: :construction: working on card products

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,36 +1,45 @@
-export {};
-// import { useMakeupApi } from '../../services';
+import { useMakeupApi, loadingAnimation } from '../../services';
 
-// const { getProducts } = useMakeupApi();
+const { getProducts } = useMakeupApi();
 
-// const {data: resProducts} = await getProducts();
-// console.log('res', res);
+loadingAnimation({ isFetching: true, querySelector: '#loading' });
 
-// const products = document.querySelector("products")!;
+const resProducts = await getProducts();
+if (resProducts.data.length > 0)
+  loadingAnimation({ isFetching: false, querySelector: '#loading' });
 
-// resProducts.data.forEach((item) => {
-//   products.innerHTML += `<tr>
-//     <th scope="row">${responseEvents.indexOf(item) + 1}</th>
-//     <td>${new Date(item.scheduled).toLocaleDateString("pt-BR")}
-//       </td>
-//     <td>${item.name}</td>
-//     <td>${item.attractions}</td>
-//     <td>
-//         <a href="reservas.html?id=${
-//           item._id
-//         }" class="btn btn-dark">ver reservas</a>
-//         <a href="editar-evento.html?id=${
-//           item._id
-//         }" class="btn btn-secondary">editar</a>
-//         <a href="excluir-evento.html?id=${
-//           item._id
-//         }" class="btn btn-danger">excluir</a>
-//     </td>
-// </tr>`;
-// });
+console.log('resProducts', resProducts);
 
-// products.innerHTML += `
-//   <div>
-//     <div></div>
-//   </div>
-// `
+const products = document.querySelector('#products')!;
+resProducts.data.forEach((item) => {
+  products.innerHTML += `
+  <div
+    id="product"
+    class="w-[250px] h-full shadow-1 rounded-1 flex flex-col justify-center items-center gap-y-5 px-5 py-8 cursor-pointer hover:shadow-0 duration-500"
+  >
+    <img
+      class="w-full"
+      src=${item.image_link}
+      onerror=this.src="./src/assets/images/default-image.png"
+    />
+    <h2 class="text-lg font-bold text-center text-text-primary">
+      ${item.name ?? ''}
+    </h2>
+    <div id="brand-price" class="flex justify-center items-center gap-x-2 w-full">
+      <p class="shadow-1 px-2 py-1 rounded-md text-text-secondary font-semibold">
+      ${item.brand ?? ''}
+      </p>
+      <p
+        class="px-2 py-1 rounded-md shadow-1 bg-primary-main text-common-white font-semibold"
+      >
+      R$ ${
+        Number(item.price).toLocaleString('pt-br', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        }) ?? 0
+      }
+      </p>
+    </div>
+  </div>
+  `;
+});
