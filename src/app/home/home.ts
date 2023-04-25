8f247ad5f20ebda54a0f68b75ea96634c6955867
AdaoBJr <adaobjr@gmail.com>
2023-04-24 23:36:20 -0400

    feat: :boom: updated index.html / home.ts with new feats

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,43 +1,63 @@
-import { useMakeupApi, loadingAnimation } from '../../services';
+import {
+  PROD_KEY,
+  useMakeupApi,
+  loadingAnimation,
+  saveInLocalStorage,
+} from '../../services';
 
 const { getProducts } = useMakeupApi();
 
 loadingAnimation({ isFetching: true, querySelector: '#loading' });
 
 const resProducts = await getProducts();
+saveInLocalStorage(PROD_KEY, resProducts.data);
+
 if (resProducts.data.length > 0)
   loadingAnimation({ isFetching: false, querySelector: '#loading' });
 
 const products = document.querySelector('#products')!;
-resProducts.data.splice(50, 63).forEach((item) => {
-  products.innerHTML += `
-  <div
-    id="product"
-    class="w-[250px] h-full shadow-1 rounded-1 flex flex-col justify-center items-center gap-y-5 px-5 py-8 cursor-pointer hover:shadow-0 duration-500"
-  >
-    <img
-      class="w-full"
-      src=${item.image_link}
-      onerror=this.src="./src/assets/images/default-image.png"
-    />
-    <h2 class="text-lg font-bold text-center text-text-primary">
-      ${item.name ?? ''}
-    </h2>
-    <div id="brand-price" class="flex justify-center items-center gap-x-2 w-full">
-      <p class="shadow-1 px-2 py-1 rounded-md text-text-secondary font-semibold">
-      ${item.brand ?? ''}
-      </p>
-      <p
-        class="px-2 py-1 rounded-md shadow-1 bg-primary-main text-common-white font-semibold"
-      >
-      R$ ${
-        Number(item.price).toLocaleString('pt-br', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 2,
-        }) ?? 0
-      }
-      </p>
-    </div>
-  </div>
-  `;
-});
+resProducts.data
+  .splice(50, 64)
+  .sort((a, b) => a.rating! - b.rating!)
+  .flatMap((item) => {
+    products.innerHTML += `
+    <a id="product-${item.id}" href="details.html?id=${item.id}" >
+      <div
+        id="product-${item.id}"
+        class="w-[250px] h-full shadow-1 rounded-1 flex flex-col justify-center items-center gap-y-5 px-5 py-8 cursor-pointer hover:shadow-0 duration-500"
+        >
+        <img
+          id="product-${item.id}"
+          class="w-full"
+          src=${item.image_link}
+          onerror=this.src="./src/assets/images/default-image.png"
+        />
+        <h2 class="text-lg font-bold text-center text-text-primary" id="product-${
+          item.id
+        }">
+          ${item.name ?? ''}
+        </h2>
+        <div id="product-${
+          item.id
+        }" class="flex justify-center items-center gap-x-2 w-full" >
+          <p class="shadow-1 px-2 py-1 rounded-md text-text-secondary font-semibold" id="product-${
+            item.id
+          }">
+          ${item.brand ?? ''}
+          </p>
+          <p
+            id="product-${item.id}"
+            class="px-2 py-1 rounded-md shadow-1 bg-primary-main text-common-white font-semibold"
+          >
+          R$ ${
+            Number(item.price).toLocaleString('pt-BR', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            }) ?? 0
+          }
+          </p>
+        </div>
+      </div>
+    </a>
+      `;
+  });
