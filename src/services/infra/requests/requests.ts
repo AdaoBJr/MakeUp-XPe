998d5c538486c277c6c13691794fbcf79714756c
AdaoBJr <adaobjr@gmail.com>
2023-04-25 23:57:10 -0400

    feat: :zap: updated requests

--- src/services/infra/requests/requests.ts
+++ src/services/infra/requests/requests.ts
@@ -5,7 +5,30 @@ import { GetProducts } from '../../../types';
 export const useMakeupApi = () => {
   const { URL_BASE } = getEnv();
 
-  const getProducts = async () => await api.get<GetProducts[]>(URL_BASE);
+  const getProducts = async () => {
+    const res = await api.get<GetProducts[]>(URL_BASE);
+    return res.data;
+  };
 
-  return { getProducts };
+  const getProductsByFilter = async ({
+    brandFilter,
+    typeFilter,
+  }: {
+    brandFilter: string | null;
+    typeFilter: string | null;
+  }) => {
+    let filters = ``;
+    if (brandFilter) {
+      filters += `&brand=${brandFilter}`;
+    }
+    if (typeFilter) {
+      filters += `&product_type=${typeFilter}`;
+    }
+    const res = await api.get<GetProducts[]>(
+      `${URL_BASE}/${filters ? `?${filters}` : ''}`
+    );
+    return res.data;
+  };
+
+  return { getProducts, getProductsByFilter };
 };
